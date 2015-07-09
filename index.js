225dc1c3250c174a62621e96af157e71d3d6e73c
Alexander Swensen <alex.swensen@gmail.com>
2015-07-09 11:19:25 -0400

    Add a basic API reference for future use.

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
 var express = require('express');
 var app = express();
 
+var myObject = [
+  {
+    name: "Billy James"
+  },
+  {
+    name: "Jane Doe"
+  }
+];
+
 app.set('port', (process.env.PORT || 5000));
 
 app.use(express.static(__dirname + '/public'));
@@ -11,6 +20,10 @@ app.set('view engine', 'ejs');
 
 app.get('/', function(request, response) {
   response.render('pages/index')
+});
+
+app.get('/api', function(request, response) {
+  response.send(JSON.stringify(myObject));
 });
 
 app.listen(app.get('port'), function() {
